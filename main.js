a0b6a3a5c593d17151599be1b9c165640defc43d
oog <78754673+TramHammer@users.noreply.github.com>
2023-09-13 17:18:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+import weather from './src/forecast.js'
+import cron from "node-cron"
+
+console.log('\x1b[35m%s\x1b[0m',`[Initalized] `,'\x1b[0m',``)
+cron.schedule('0 7 * * *', () => {
+    weather.runMorning()
+});
+
+cron.schedule('1 12 * * *', () => {
+    weather.runAfternoon()
+});
+
+cron.schedule('0 18 * * *', () => {
+    weather.runAfternoon()
+});
+
+
