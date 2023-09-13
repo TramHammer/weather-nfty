f8717c133338fa5094020468cf42ff0f1282d4b8
oog <78754673+TramHammer@users.noreply.github.com>
2023-09-13 17:59:26 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import weather from './src/forecast.js'
+import express from './src/express.js'
 import cron from "node-cron"
 
 console.log('\x1b[35m%s\x1b[0m',`[Initalized] `,'\x1b[0m',``)
+
+express.uptimeKumaIntegration()
+
 cron.schedule('0 7 * * *', () => {
     weather.runMorning()
 });
