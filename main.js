20780dd4d53561f76c91b05650577a0888eebd3a
TramHammer <78754673+TramHammer@users.noreply.github.com>
2023-10-30 18:39:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ import cron from "node-cron"
 const topic = '' //nfty topic link
 const lat = "" //should be rounded to the 4th decimal place ex. -88.5448
 const long = "" //should be rounded to the 4th decimal place
-const retryAttempts = 4; //change this to a reasonable number of times you want it to retry until it determines that there is no proper connection to the API
+const retryAttempts = 4 //change this to a reasonable number of times you want it to retry until it determines that there is no proper connection to the API
+const shortAlerts = true //change if you want the short headline or the full alert details(the full details will not show in the notification bubble completely)
 
-weather.setConfig(lat, long, topic, retryAttempts)
+weather.setConfig(lat, long, topic, retryAttempts, shortAlerts)
 //This is optional for uptime Kuma Integration
 express.uptimeKumaIntegration(5000)
 
@@ -27,4 +28,10 @@ cron.schedule('0 12 * * *', () => {
 cron.schedule('0 18 * * *', () => {
     weather.runAfternoon()
 });
+
+//This would check for alerts every 10 minutes
+cron.schedule('*/10 * * * *', () => {
+    weather.getAlerts()
+})
+
 
