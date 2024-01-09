859c4040d7313c72b2fc4420d8bdb3ce96df4844
TramHammer <78754673+TramHammer@users.noreply.github.com>
2024-01-09 02:12:25 -0500

    Update forecast.js

--- src/forecast.js
+++ src/forecast.js
@@ -1,89 +1,112 @@
-let now = new Date()
+import fetch from 'node-fetch'
 
 let topic = ""
 let locationGridPoint = ""
 let cityLocation = ""
 let location = ""
 let alertLocation = ""
-let retrys = 0;
 let alerts = []
 let link = ""
 let shortAlert = null
+let pushMobileError = null
 
 export default { setConfig, initializeGridPointData, runAfternoon, runMorning, getAlerts, clearAlertTable }
 
-function setConfig(latitude, longitude, nftyTopic, retryAttempts, shortAlerts) {
+/**
+ * Sets the configuration within this file 
+ * @param {*} latitude 
+ * @param {*} longitude 
+ * @param {*} nftyTopic 
+ * @param {*} shortAlerts 
+ * @param {*} pushMobileErrors 
+ */
+function setConfig(latitude, longitude, nftyTopic, shortAlerts, pushMobileErrors) {
     topic = nftyTopic
     shortAlert = shortAlerts
-    retrys = retryAttempts
+    pushMobileError = pushMobileErrors
+    latitude = Number(latitude).toFixed(4)
+    longitude = Number(longitude).toFixed(4)
+    console.log(latitude, longitude)
     link = "https://forecast.weather.gov/MapClick.php?lat=" + latitude + "&lon=" + longitude
     location = "https://api.weather.gov/points/" + latitude + "," + longitude
     alertLocation = "https://api.weather.gov/alerts/active?point=" + latitude + "," + longitude
-    console.log('\x1b[35m%s\x1b[0m',`[Initalized] `,'\x1b[0m',``)
+    console.log('\x1b[32m%s\x1b[0m', `[setConfig] `, '\x1b[0m', `${new Date().toISOString()} Initalized`)
 }
 
+/**
+ * Clears alert table when called
+ */
 function clearAlertTable() {
     alerts = []
 }
 
-/*
-Establishes the correct gridPoint based on the latitude and longitude based on the passed values in setConfig
-*/
+/**
+ * Establishes the correct gridPoint based on the latitude and longitude based on the passed values in setConfig
+ */
 async function initializeGridPointData() {
-    let requestCount = 0;
-    do {
+    try {
         const request = await fetch(location, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
             }
         })
-        if (!request.ok) {
-            requestCount++
-            continue
-        } else {
-            console.log('\x1b[35m%s\x1b[0m', `[getPreliminaryData] `, '\x1b[0m', "Network Response is OK\nStatus: ", request.status)
-            const data = await request.json()
-            locationGridPoint = JSON.stringify(data.properties.forecast).replace(/['"]+/g, '')
-            cityLocation = JSON.stringify(data.properties.relativeLocation.properties.city + ", " + data.properties.relativeLocation.properties.state).replace(/['"]+/g, '')
-            return true
-        }
-    } while (requestCount < retrys)
-    fetch(topic, {
-        method: 'POST',
-        headers: {
-            'Title': "Network response is not OK",
-            'Priority': "4"
-        },
-        body: "initializeGridPointData() failed after " + retrys
-    })
-    return false
+        console.log('\x1b[35m%s\x1b[0m', `[initializeGridPointData] `, '\x1b[0m', `${new Date().toISOString()} Network Response is OK\nStatus: `, request.status)
+        const data = await request.json()
+        locationGridPoint = JSON.stringify(data.properties.forecast).replace(/['"]+/g, '')
+        cityLocation = JSON.stringify(data.properties.relativeLocation.properties.city + ", " + data.properties.relativeLocation.properties.state).replace(/['"]+/g, '')
+    } catch (err) {
+        postErrorNotification("initializeGridPointData", err)
+    }
 }
-/*
-Gets the forecast for morning, afternoon, and the next day
-*/
+
+/**
+ * Posts error as a notification through the same channel in nfty
+ * @param {*} messageType the function name
+ * @param {*} err the error from the catch statement
+ */
+async function postErrorNotification(messageType, err) {
+    console.log('\x1b[31m%s\x1b[0m', `[postErrorNotification]`, '\x1b[0m', ` ${new Date().toISOString()} ${messageType}() failed, ${err}}`)
+    if (pushMobileError) {
+        await fetch(topic, {
+            method: 'POST',
+            headers: {
+                'Title': "Network response is not OK",
+                'Priority': "4"
+            },
+            body: `${messageType}() failed, see logs for more details...`
+        }).catch(err => console.log('\x1b[31m%s\x1b[0m', `[postErrorNotification]`, '\x1b[0m', ` ${new Date().toISOString()} Failed to send mobile push notification, ${err}`))
+    }
+}
+
+/**
+ * Gets the forecast for morning, afternoon, and the next day
+ */
 async function runMorning() {
     getForecast(0)
     getForecast(1)
     getForecast(2)
-    console.log('\x1b[35m%s\x1b[0m', `[PUSH] `, '\x1b[0m', `Successfully sent message ${now.toDateString()}`)
+    console.log('\x1b[35m%s\x1b[0m', `[runMorning] `, '\x1b[0m', ` ${new Date().toISOString()} Successfully sent message`)
 }
-/*
-Gets the forecast for the evening
-*/
+
+/**
+ * Gets the forecast for the evening
+ */
 async function runAfternoon() {
     getForecast(0)
-    console.log('\x1b[35m%s\x1b[0m', `[PUSH] `, '\x1b[0m', `Successfully sent message ${now.toDateString()}`)
-
+    console.log('\x1b[35m%s\x1b[0m', `[runAfternoon] `, '\x1b[0m', ` ${new Date().toISOString()} Successfully sent message`)
 }
 
-/*
-nfty post notification method, designed for Andriod w/support for Icon
-*/
+
+/**
+ * nfty post notification method, designed for Andriod w/support for Icon
+ * @param {*} forecast the forecast product as a dictionary
+ * @param {*} type if it is an api.weather.gov alert
+ */
 async function postNotification(forecast, type) {
     if (type) {
         if (!shortAlert) {
-            fetch(topic, {
+            await fetch(topic, {
                 method: 'POST',
                 headers: {
                     'Icon': "https://www.shareicon.net/data/512x512/2015/08/18/86945_warning_512x512.png",
@@ -93,9 +116,12 @@ async function postNotification(forecast, type) {
                     'Actions': `view, Open weather.gov, ${link}`
                 },
                 body: forecast.description + "\n\n" + forecast.instructions
-            })
+            }).then(() => {
+                return false
+
+            }).catch(err => console.log(err))
         } else {
-            fetch(topic, {
+            await fetch(topic, {
                 method: 'POST',
                 headers: {
                     'Icon': "https://www.shareicon.net/data/512x512/2015/08/18/86945_warning_512x512.png",
@@ -105,10 +131,13 @@ async function postNotification(forecast, type) {
                     'Actions': `view, Open weather.gov, ${link}`
                 },
                 body: forecast.headline + "\n\n" + forecast.instructions
-            })
+            }).then(() => {
+                return false
+
+            }).catch(err => console.log(err))
         }
     } else {
-        fetch(topic, {
+        await fetch(topic, {
             method: 'POST',
             headers: {
                 'Icon': forecast.icon,
@@ -116,122 +145,105 @@ async function postNotification(forecast, type) {
                 'Priority': "3"
             },
             body: forecast.temperature + "°F • " + forecast.shortForecast + " • " + forecast.wind
-        }) 
+        }).then(() => {
+            return false
+
+        }).catch(err => console.log(err))
     }
 }
 
-/*
-Gets alert information based on alert link
-*/
+
+/**
+ * Gets alert information based on alert link
+ * @returns 
+ */
 async function getAlerts() {
-    let requestCount = 0;
-    do {
+    try {
         const request = await fetch(alertLocation, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
             }
         })
-        if (!request.ok) {
-            console.log("Failed " + requestCount + " times " + location)
-            requestCount++
-            continue;
-        } else {
-            console.log('\x1b[35m%s\x1b[0m', `[getAlerts()] `, `Network Response is OK : getAlerts()\nStatus: `, request.status)
-            const data = await request.json()
 
-            for (let i = 0; i < Object.keys(data.features).length; i++) {
-                let information = data.features[i].properties;
+        console.log('\x1b[35m%s\x1b[0m', `[getAlerts] `, ` ${new Date().toISOString()} Network Response is OK : getAlerts()\nStatus: `, request.status)
+        const data = await request.json()
 
-                const alert = {
-                    "name": JSON.stringify(information.event).replace(/['"]+/g, ''),
-                    "description": (information.description),
-                    "instructions":  (information.instruction),
-                    "headline": JSON.stringify(information.headline).replace(/['"]+/g, ''),
-                    "effective":  JSON.stringify(information.effective).replace(/['"]+/g, ''),
-                    "expires":  JSON.stringify(information.expires).replace(/['"]+/g, ''),
-                    "sender":  JSON.stringify(information.senderName).replace(/['"]+/g, ''),
-                    "id": JSON.stringify(information.id).replace(/['"]+/g, ''),
-                }
-                if (!alerts.includes(alert.id)) {
-                    postNotification(alert, true)
-                    alerts.push(alert.id)
-                }
+        for (let i = 0; i < Object.keys(data.features).length; i++) {
+            let information = data.features[i].properties;
+
+            const alert = {
+                "name": JSON.stringify(information.event).replace(/['"]+/g, ''),
+                "description": (information.description),
+                "instructions": (information.instruction),
+                "headline": JSON.stringify(information.headline).replace(/['"]+/g, ''),
+                "effective": JSON.stringify(information.effective).replace(/['"]+/g, ''),
+                "expires": JSON.stringify(information.expires).replace(/['"]+/g, ''),
+                "sender": JSON.stringify(information.senderName).replace(/['"]+/g, ''),
+                "id": JSON.stringify(information.id).replace(/['"]+/g, ''),
             }
-            return true
+            if (!alerts.includes(alert.id)) {
+                postNotification(alert, true)
+                alerts.push(alert.id)
+            }
         }
-    } while (requestCount < retrys)
-    fetch(topic, {
-        method: 'POST',
-        headers: {
-            'Title': "Network response is not OK",
-            'Priority': "4"
-        },
-        body: "getForecast() failed after " + retrys
-    })
-    return false
-
+        return true
+    } catch (err) {
+        postErrorNotification("getAlerts", err)
+        return false
+    }
 }
 
 
-/*
-Gets the forecast based on the period using locationGridPoint
-*/
+/**
+ * Gets the forecast based on the period using locationGridPoint
+ * @param {Number} period the period to fetch forecast data from
+ * @returns 
+ */
 async function getForecast(period) {
-    let requestCount = 0;
-    do {
+    try {
         const request = await fetch(locationGridPoint, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
             }
         })
-        if (!request.ok) {
-            console.log("Failed " + requestCount + " times " + location)
-            requestCount++
-            continue
-        } else {
-            console.log('\x1b[35m%s\x1b[0m', `[getForecast] `, `Network Response is OK : getForecast(${period})\nStatus: `, request.status)
-            const data = await request.json()
-            let prop = data.properties.periods[0]
-            switch (period) {
-                case 0:
-                    prop = data.properties.periods[0]
-                    break
-                case 1:
-                    prop = data.properties.periods[1]
-                    break
-                case 2:
-                    prop = data.properties.periods[2]
-                    break
-                case 3:
-                    prop = data.properties.periods[3]
-                    break
-                case 4:
-                    prop = data.properties.periods[4]
-                    break
-            }
-
-            const forecast = {
-                "name": JSON.stringify(prop.name).replace(/['"]+/g, ''),
-                "dew": JSON.stringify(prop.dewpoint.value).replace(/['"]+/g, ''),
-                "temperature": JSON.stringify(prop.temperature).replace(/['"]+/g, ''),
-                "relativeHumidity": JSON.stringify(prop.relativeHumidity.value + "%").replace(/['"]+/g, ''),
-                "wind": JSON.stringify(prop.windDirection + " " + prop.windSpeed).replace(/['"]+/g, ''),
-                "icon": JSON.stringify(prop.icon).replace(/['"]+/g, ''),
-                "shortForecast": JSON.stringify(prop.shortForecast).replace(/['"]+/g, '')
-            }
-            postNotification(forecast, false)
-            return true
+        console.log('\x1b[35m%s\x1b[0m', `[getForecast] `, ` ${new Date().toISOString()} Network Response is OK : getForecast(${period})\nStatus: `, request.status)
+        const data = await request.json()
+        let prop = data.properties.periods[0]
+        switch (period) {
+            case 0:
+                prop = data.properties.periods[0]
+                break
+            case 1:
+                prop = data.properties.periods[1]
+                break
+            case 2:
+                prop = data.properties.periods[2]
+                break
+            case 3:
+                prop = data.properties.periods[3]
+                break
+            case 4:
+                prop = data.properties.periods[4]
+                break
         }
-    } while (requestCount < retrys)
-    fetch(topic, {
-        method: 'POST',
-        headers: {
-            'Title': "Network response is not OK",
-            'Priority': "4"
-        },
-        body: "getForecast() failed after " + retrys
-    })
-    return false
+
+        const forecast = {
+            "name": JSON.stringify(prop.name).replace(/['"]+/g, ''),
+            "dew": JSON.stringify(prop.dewpoint.value).replace(/['"]+/g, ''),
+            "temperature": JSON.stringify(prop.temperature).replace(/['"]+/g, ''),
+            "relativeHumidity": JSON.stringify(prop.relativeHumidity.value + "%").replace(/['"]+/g, ''),
+            "wind": JSON.stringify(prop.windDirection + " " + prop.windSpeed).replace(/['"]+/g, ''),
+            "icon": JSON.stringify(prop.icon).replace(/['"]+/g, ''),
+            "shortForecast": JSON.stringify(prop.shortForecast).replace(/['"]+/g, '')
+        }
+        postNotification(forecast, false)
+        return true
+    } catch (err) {
+        postErrorNotification("getForecast", err)
+        return false
+    }
 }
+
+
